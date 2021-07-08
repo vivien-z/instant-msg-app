4208fa9dc6773332b61e9d54ce901ab5ea9b6563
vivien zhu <yuanzyy@gmail.com>
2021-07-08 17:14:52 -0400

    set input field for messages

--- client/src/components/OpenChatWindow.js
+++ client/src/components/OpenChatWindow.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, InputGroup, Button } from 'react-bootstrap'
+import { useChatrooms } from '../contexts/ChatroomsProvider';
 
 export default function OpenChatWindow() {
+  const [message, setMessage] = useState('')
+  const { sendMessage, selectedChatroom } = useChatrooms()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const receiverIds = selectedChatroom.roomUsers.map(user => user.id)
+
+    sendMessage(receiverIds, message)
+    setMessage('')
+  }
+
   return (
-    <div>
-      Open Chat Window
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto">
+
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="m-2">
+          {/*// send button to be attached with the form*/}
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+              style={{ height: '75px', resize: 'none'}}
+              required
+            />
+            <Button type="submit">Send</Button>
+          </InputGroup>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
+            // <InputGroup.Append>
+            // </InputGroup.Append>
