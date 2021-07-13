b9f93a14fdc54e9bf55ff02bf47c6e9a64e1cf4f
vivien zhu <yuanzyy@gmail.com>
2021-07-13 16:14:13 -0400

    adjust navtab to left side vertical

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Tab, Nav, Button, Modal } from 'react-bootstrap'
+import { Tab, Nav, Button, Modal, Row, Col } from 'react-bootstrap'
 import Chatrooms from './Chatrooms'
 import Contacts from './Contacts'
 import NewChatroomModal from './NewChatroomModal'
@@ -7,7 +7,7 @@ import NewContactModal from './NewContactModal'
 import '../styles/global.css'
 import styles from '../styles/Sidebar.module.css';
 
-const CHATROOMS_KEY = "chatrooms"
+const CHATROOMS_KEY = "Chats"
 const CONTACTS_KEY = "Contacts"
 
 export default function Sidebar({ myId, myUsername }) {
@@ -25,31 +25,40 @@ export default function Sidebar({ myId, myUsername }) {
       <div className={`${styles.profile} purple-bg p-3 border-bottom`}>
         <span className={`${styles.avatar}`}>{myUsername[0].toUpperCase()}</span><span className="">{myUsername}</span>
       </div>
+
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+        <Row>
+          <Col sm={3}>
+            <Nav variant="tabs" className={`${styles.navTab} justify-content-center align-items-start`}>
+              <Nav.Item>
+                <Nav.Link eventKey={CHATROOMS_KEY}>Chats</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content className='border-right overflow-auto flex-grow-1'>
+              <Tab.Pane eventKey={CHATROOMS_KEY}>
+                <Chatrooms myUsername={myUsername} />
+              </Tab.Pane>
+              <Tab.Pane eventKey={CONTACTS_KEY}>
+                <Contacts myUsername={myUsername} />
+              </Tab.Pane>
+            </Tab.Content>
 
-        <Nav variant="tabs" className='justify-content-center'>
-          <Nav.Item>
-            <Nav.Link eventKey={CHATROOMS_KEY}>Chatrooms</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
-          </Nav.Item>
-        </Nav>
+          </Col>
 
-        <Tab.Content className='border-right overflow-auto flex-grow-1'>
-          <Tab.Pane eventKey={CHATROOMS_KEY}>
-            <Chatrooms myUsername={myUsername} />
-          </Tab.Pane>
-          <Tab.Pane eventKey={CONTACTS_KEY}>
-            <Contacts myUsername={myUsername} />
-          </Tab.Pane>
-        </Tab.Content>
+        </Row>
+
         <Button className='rounded-0' onClick={()=>setModalOpen(true)}>
           New {btnName}
         </Button>
       </Tab.Container>
+
       <Modal show={modalOpen} onHide={closeModal}>
-        {activeKey === "chatrooms" ?
+        {activeKey === "Chats" ?
           <NewChatroomModal closeModal={closeModal} myId={myId} myUsername={myUsername}/> :
           <NewContactModal closeModal={closeModal}/>
         }
