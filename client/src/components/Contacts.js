9e1580cc36d269c1315da3f3da69ea43ff0fbf96
vivien zhu <yuanzyy@gmail.com>
2021-11-16 16:12:09 -0500

    adjusted Contact window user list

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -3,21 +3,17 @@ import { useUsers } from '../contexts/UsersProvider';
 
 export default function Contacts({ myUsername }) {
   const { users } = useUsers()
-  const contacts = users.filter(user => {
-    return user.username !== myUsername
-  })
+  const contacts = users.find(user => user.username === myUsername).contacts
 
   return (
     <ListGroup variant="flush">
 
         {contacts.map((contact, i) => (
           <ListGroup.Item key={i}>
-              {contact.username}
-              {/*<div className="px-2 text-muted small">Id: {contact.id}</div>*/}
+              {users.find(user => user.id === contact.id).username}
           </ListGroup.Item>
         ))}
 
     </ListGroup>
   )
 }
-
