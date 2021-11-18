82a891e084ca2205fcd2e0c17a21e6f9b9ffebdd
vivien zhu <yuanzyy@gmail.com>
2021-11-18 11:57:17 -0500

    reset file to serve as server

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const path = require('path') //(router)
+const app = express()
+
+const http = require('http')
+const { Server }= require("socket.io")
+const server = http.createServer(app)
+// const cors = require('cors')
+
+const PORT = process.env.PORT || 8080
+
+const io = new Server(server, {
+  cors: {
+    origin: ["https://619548b7832b0a000794243e--instant-msg-page.netlify.app/", "http://localhost:3000"],
+    // origin: *,
+    methods: ["GET", "POST", "PUT"],
+    credentials: true
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'build')))
+// // app.use(cors())
+// app.use(cors({
+//     origin: ["https://619548b7832b0a000794243e--instant-msg-page.netlify.app/", "http://localhost:3000"],
+//     methods: ["GET", "POST", "PUT"],
+// }))
+
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.send('Server is up and running')
+});
+
+
+io.on('connection', (socket) => {
+  console.log('We have a new connection.')
+
+  socket.on('add-new-user', ({id, username}) => {
+    socket.broadcast.emit('new-user-created', ({id, username}))
+    console.log('new-user-created')
+  })
+
+  socket.on('send-message', ({ selectedChatroom, sender, msgText, roomUsers }) => {
+    socket.broadcast.emit('receive-message', {selectedChatroom, sender, msgText})
+  })
+
+  socket.on('disconnect', () => {
+    console.log('A user had left.')
+  })
+})
+
+server.listen(PORT, () => console.log(`Server is listening to port ${PORT}`))
+
+
+// const io = require('socket.io')(5000, {
+
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT"],
+//     credentials: true
+//   }
+// })
+
+// io.on('connection', socket => {
+//   // const id = socket.handshake.query.myId
+//   // socket.join(id)
+
+//   // socket.on('create-new-chatroom', ({ roomUserIds }) => {
+//   //   socket.broadcast.emit('match-new-chatroom', { roomUserIds })
+//   // })
+
+// })
+
