b0985b4bfd0cd6523d16e4e292c18aaced32d942
vivien zhu <yuanzyy@gmail.com>
2021-11-19 11:19:07 -0500

    use cors() with dynamic origin setting

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require('express')
 const path = require('path') //(router)
-const app = express()
 const cors = require('cors')
+const app = express()
 
 const http = require('http')
 const { Server }= require("socket.io")
 const server = http.createServer(app)
-// const netlifyLink = 'https://instant-msg-page.netlify.app/'
 
 const PORT = process.env.PORT || 8080
 
-const io = new Server(server, {
-  // cors: {
-  //   origin: 'https://instant-msg-page.netlify.app/',
-  //   // origin: [netlifyLink, "http://localhost:3000"],
-  //   methods: ["GET", "POST", "HEAD", "OPTIONS"],
-  //   allowedHeaders: ["instant-msg-page", "Origin"],
-  //   credentials: true
-  // }
-});
+const io = new Server(server)
+// , {
+//   // cors: {
+//   //   origin: 'https://instant-msg-page.netlify.app/',
+//   //   // origin: [netlifyLink, "http://localhost:3000"],
+//   //   methods: ["GET", "POST", "HEAD", "OPTIONS"],
+//   //   allowedHeaders: ["instant-msg-page", "Origin"],
+//   //   credentials: true
+//   // }
+// });
 
-app.use(cors({
-    origin: 'https://instant-msg-page.netlify.app/',
-    methods: ["GET", "POST", "HEAD", "OPTIONS"],
-    // allowedHeaders: ["instant-msg-page", "Origin"],
-    credentials: true
-}))
+const whiteList = ['https://instant-msg-page.netlify.app/', 'http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// app.use(cors({
+//     origin: 'https://instant-msg-page.netlify.app/',
+//     methods: ["GET", "POST", "HEAD", "OPTIONS"],
+//     // allowedHeaders: ["instant-msg-page", "Origin"],
+//     credentials: true
+// }))
+app.options('*', cors())
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', (req, res, next) => {
+app.get('/*', cors(corsOptions), (req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.send('CORS-enabled for a whitelisted domain')
   res.send('Server is up and running')
 });
 
